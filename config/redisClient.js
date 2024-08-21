2f8477ad732922c24f1f6d7889f3dd9246b39799
japhlett <jatakyiwaa001@st.ug.edu.gh>
2024-08-21 17:07:00 +0000

    make changes to redis config file

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,17 +1,21 @@
-import { createClient } from 'redis';
+import redis from 'redis';
 
-export const redisClient= createClient({
-    url: 'redis://127.0.0.1:6379'  // Adjust if needed
+const REDIS_PORT = 6379;
+
+export const redisClient = redis.createClient({
+    url: `redis://localhost:${REDIS_PORT}`
 });
 
 redisClient.on('error', (err) => {
-    console.error('Redis error:', err);
+    console.error('Redis Client Error:', err);
 });
 
-redisClient.on('connect', () => {
-    console.log('Connected to Redis');
-});
-
-redisClient.connect();  
+redisClient.connect()
+    .then(() => {
+        console.log('Connected to Redis')
+    })
+    .catch((err) => {
+        console.error('Failed to connect to Redis:', err)
+    });
 
 
