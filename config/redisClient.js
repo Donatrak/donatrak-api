ccaeefa2145f259d56647e4d646bd42eed8982ef
japhlet <jatakyiwaa001@st.ug.edu.gh>
2024-08-21 15:49:49 +0000

    Update redisClient.js

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 export const redisClient= createClient({
-    url: 'redis://127.0.0.1:6379'  // Adjust if needed
+    url:  process.env.REDIS_URL 
 });
 
 redisClient.on('error', (err) => {
